b6cfbebe8c85b6dad2d5f23c6fd0afe3f95a34bb
Oliwia Brożek <oliwiabrozek@gmail.com>
2020-04-07 19:08:40 +0200

    Zabezpieczenie przed manipulacją daty utworzenia zadania (#32)
    * Usunięcie daty utworzenia z modelu (datę utworzenia można odczytać z _id)
    
    * Dodanie timestampa utworzenia

--- backend/src/models/task.ts
+++ backend/src/models/task.ts
@@ -11,14 +11,15 @@ export interface TaskInterface extends Document {
 }
 
 export const TaskSchema: Schema = new Schema({
-    title: {type: String, required: true},
-    details: {type: String, required: false},
-    inProgress: {type: Boolean, required: true},
-    priority: {type: String, required: true},
-    // TODO: disable custom creation date (use default always)
-    creationDate: {type: Date, default: () => Date.now()},
-    deadlineDate: {type: Date, required: false},
-});
+        title: {type: String, required: true},
+        details: {type: String, required: false},
+        inProgress: {type: Boolean, required: true},
+        priority: {type: String, required: true},
+        deadlineDate: {type: Date, required: false},
+    },
+    {
+        timestamps: {createdAt: true, updatedAt: false}
+    });
 
 const Task = mongoose.model<TaskInterface>("Task", TaskSchema);
 export default Task;
