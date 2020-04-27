5fee69c0214010899a14b748624e99d1f3241575
Bartłomiej Szlachta <37980016+bartlomiej-szlachta@users.noreply.github.com>
2020-04-27 15:17:50 +0200

    Użycie expo (#58)
    * expo v0
    
    * ooooooooooooo
    
    * usunięcie opcji z deviceid
    
    * Przeniesienie adresu url api do osobnego pliku
    
    * Cofnięcie zmiany konfiguracji typescript
    
    * Zmiana uzyskiwania id urządzenia
    
    * Cofnięcie zmian w App.tsx
    
    * Cofnięcie zmian w babel.config.js

--- client/src/utils/APIClient.ts
+++ client/src/utils/APIClient.ts
@@ -1,9 +1,9 @@
-import DeviceInfo from 'react-native-device-info';
-
 const baseUrl = 'http://3.88.167.229:5000';
 
 const getDeviceId = (): string => {
-  return DeviceInfo.getUniqueId();
+  // @ts-ignore
+  return Expo.Constants.deviceId;
 };
+
 
 export const urlTasks = `${baseUrl}/api/devices/${getDeviceId()}/tasks/`;
