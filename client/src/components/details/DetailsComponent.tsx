9ac847bdf37bba758e3f0528db8459ae03f0f17e
Kamil Rusin <lotrusek@gmail.com>
2020-04-28 19:12:34 +0200

    Poprawa widoku taska

--- client/src/components/details/DetailsComponent.tsx
+++ client/src/components/details/DetailsComponent.tsx
@@ -4,8 +4,9 @@ import {ActivityIndicator, TextInput, Text, RadioButton, Snackbar} from 'react-n
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {timeConverter, dateConverter} from '../../utils/dateTimeConverter';
-import {colors, fonts, margin} from "../../styles/common";
+import {colors, fonts, margin, padding} from "../../styles/common";
 import {useTheme} from "@react-navigation/native";
+import SectionHeader from "../_components/SectionHeader";
 
 interface Props {
   handleConfirm: (data: Date) => void,
@@ -54,6 +55,21 @@ const DetailsComponent = (props: Props) => {
 
   const theme = useTheme();
 
+  const radioOptions = [
+    {
+      label: 'Low',
+      value: 'low',
+    },
+    {
+      label: 'Medium',
+      value: 'medium',
+    },
+    {
+      label: 'High',
+      value: 'high',
+    },
+  ];
+
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -67,6 +83,22 @@ const DetailsComponent = (props: Props) => {
       ),
     });
   }, [navigation]);
+
+  const prioritySelection = (
+      <View style={styles.container}>
+        <RadioButton.Group
+            onValueChange={onRadioButtonClick}
+            value={task.priority}
+        >
+          {radioOptions.map(({ label, value }, key) => (
+              <View style={styles.radioButtonWithLabel} key={key}>
+                <Text theme={theme} style={styles.radioOptionText}>{label}</Text>
+                <RadioButton theme={theme} value={value} color={colors.primary}/>
+              </View>
+          ))}
+        </RadioButton.Group>
+      </View>
+  );
 
   return (
     !isLoading ? (
@@ -102,14 +134,14 @@ const DetailsComponent = (props: Props) => {
             <View style={styles.row}>
               <TouchableOpacity onPress={onIconClick}>
                 <MaterialCommunityIcons
-                  size={36}
+                  size={48}
                   name={'timetable'}
                   color={theme.colors.primary}
                 />
               </TouchableOpacity>
               <TouchableOpacity onPress={onClearIconClick}>
                 <MaterialCommunityIcons
-                  size={36}
+                  size={48}
                   name={'close-box-outline'}
                   color={theme.colors.primary}
                 />
@@ -123,24 +155,9 @@ const DetailsComponent = (props: Props) => {
               onCancel={onCancelClick}
             />
 
-            <View style={styles.radioButtonsContainer}>
-              <RadioButton.Group
-                onValueChange={onRadioButtonClick}
-                value={task.priority}
-              >
-                <View style={styles.radioButtonLabel}>
-                  <Text theme={theme}>Low</Text>
-                  <RadioButton theme={theme} color={theme.colors.primary} value="low"/>
-                </View>
-                <View style={styles.radioButtonLabel}>
-                  <Text theme={theme}>Normal</Text>
-                  <RadioButton theme={theme} color={theme.colors.primary} value="normal"/>
-                </View>
-                <View style={styles.radioButtonLabel}>
-                  <Text theme={theme}>High</Text>
-                  <RadioButton theme={theme} color={theme.colors.primary} value="high"/>
-                </View>
-              </RadioButton.Group>
+            <View style={styles.section}>
+              <SectionHeader text="Choose priority"/>
+              {prioritySelection}
             </View>
 
           </ScrollView>
@@ -167,32 +184,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  radioButtonsContainer: {
-    display: 'flex',
+  radioButtonWithLabel: {
+    flex: 1,
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: margin.sm,
+    padding: 5,
+    paddingLeft: padding.md,
+    paddingRight: padding.md,
+    marginBottom: 2,
+    fontFamily: fonts.primary,
   },
-  radioButtonLabel: {
-    alignContent: 'space-around',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: margin.sm,
-    marginLeft: margin.sm,
+  radioOptionText: {
+    flex: 1,
+    alignSelf: 'center',
+    fontSize: fonts.md,
+    fontFamily: fonts.primary,
   },
   row: {
     display: 'flex',
     flexDirection: 'row',
     alignContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    margin: 10,
   },
   saveButton: {
     color: colors.white,
   },
   saveButtonContainer: {
     marginRight: margin.md,
+  },
+  section: {
+    marginTop: margin.vsm,
+    marginBottom: margin.sm,
   },
   snackbar: {
     position: 'absolute',
@@ -202,6 +225,7 @@ const styles = StyleSheet.create({
   textInput: {
     margin: margin.sm,
     fontFamily: fonts.primary,
+    fontSize: fonts.md,
   },
 });
 
